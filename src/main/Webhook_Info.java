b909effaed4244debe43f1d147c537e362a2df92
Blag <atejada@gmail.com>
2023-08-31 17:28:27 -0400

    Create Webhook_Info.java

---
+++ src/main/Webhook_Info.java
@@ -0,0 +1,11 @@
+import lombok.Data;
+
+@Data
+public class Webhook_Info {
+    private String id;
+    private String date;
+    private String title;
+    private String description;
+    private String participants;
+    private String status;
+}
